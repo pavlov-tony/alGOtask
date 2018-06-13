5985906c37c4c6f56a9bb23fbf99a4a5412f4c25
Anton Pavlov <pavlov.tony@gmail.com>
2018-06-13 17:14:20 +0300

    Fix resources handling

--- example.go
+++ example.go
@@ -18,51 +18,40 @@ import (
 func main() {
 	start := time.Now()
 	vocabulary := trie.InitTrie()
-
-	file, err := os.Open("vocabulary.txt")
+	vocabularyFile, err := os.Open("vocabulary.txt")
+	defer vocabularyFile.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(vocabularyFile)
 	for scanner.Scan() {
 		vocabulary.Insert(strings.ToLower(scanner.Text()))
 	}
 	if err := scanner.Err(); err != nil {
 		log.Println(err)
 	}
-	file.Close()
-
 	if len(os.Args) < 2 {
-		log.Fatal(errors.New("get command line args: The system cannot find the command line arg for input data file name."))
+		log.Fatal(errors.New("get command line args: The system cannot find the command line arg for input data file name"))
 	}
-
 	inputFileName := os.Args[1]
-
-	file, err = os.Open(inputFileName)
+	textFile, err := os.Open(inputFileName)
+	defer textFile.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	text, err := ioutil.ReadAll(file)
+	text, err := ioutil.ReadAll(textFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	file.Close()
-
 	words := strings.Split(string(text), " ")
-
 	ch := make(chan int)
-
 	for _, word := range words {
 		go search.SearchDistance(vocabulary, strings.TrimSpace(word), ch)
 	}
-
 	result := 0
-
 	for i := 0; i < len(words); i++ {
 		result += <-ch
 	}
-
 	fmt.Println(result)
 	elapsed := time.Since(start)
 	log.Printf("Search distance took %s", elapsed)
